963088f78ecd95bc7d6a4b5dc64b9e78796c16ee
AndyAtari <andrewsalteri@gmail.com>
2021-06-25 15:23:20 -0400

    Added template for welcome/<name>

--- welcome/urls.py
+++ welcome/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from welcome import views 
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("welcome/<name>", views.welcome_there, name="welcome_there"),
 ]
--- welcome/views.py
+++ welcome/views.py
@@ -1,4 +1,17 @@
+import re
+from django.utils.timezone import datetime
 from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 def home(request):
     return HttpResponse("Welcome, Trooper!")
+
+def welcome_there(request, name):
+    return render(
+        request,
+        'welcome/welcome_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
